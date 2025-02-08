1ffaa0df39912737cfc9e7dd9e4a65dd81e7f584
Rohit <rohitamar@gmail.com>
2025-02-08 10:32:31 -0500

    first

---
+++ ndarray.hpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+template<typename T>
+class NDarray {
+    public:
+
+    private:
+        std::vector<T> data_;
+};
---
+++ playground.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+x = np.ones((50, 10, 700))
+
+print(x)
